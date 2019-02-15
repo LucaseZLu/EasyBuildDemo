9a85e9282cbd693b0b5006020c941a527606543c
laizhonglu <571080414@qq.com>
2019-02-15 18:28:03 +0800

    完善

--- Assets/Demo/Scripts/GameManager.cs
+++ Assets/Demo/Scripts/GameManager.cs
@@ -1,17 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
 
-    private void Start()
-    {
-        
-    }
-
     private void Update()
     {
-        
+        if (Input.GetKey(KeyCode.F2))
+        {
+            if(SceneManager.GetActiveScene().buildIndex==0)
+                SceneManager.LoadScene(1);
+            else
+                SceneManager.LoadScene(0);
+        }
     }
 }
